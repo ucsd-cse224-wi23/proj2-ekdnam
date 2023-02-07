b68cfb19eb543be9b6ca74cde00811e0cf717407
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2023-02-06 23:31:03 -0800

    restructure code

--- tritonhttp/server.go
+++ tritonhttp/server.go
@@ -7,9 +7,7 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"mime"
 	"net"
-	"net/textproto"
 	"os"
 	"path/filepath"
 	"strings"
@@ -389,28 +387,28 @@ func (s *Server) parseAndGenerateResponse(req *Request, res *Response) error {
 // canonical key for "content-type" is "Content-Type".
 // You should store header keys in the canonical format
 // in internal data structures.
-func CanonicalHeaderKey(s string) string {
-	return textproto.CanonicalMIMEHeaderKey(s)
-}
+// func CanonicalHeaderKey(s string) string {
+// 	return textproto.CanonicalMIMEHeaderKey(s)
+// }
 
-// FormatTime formats time according to the HTTP spec.
-// It is like time.RFC1123 but hard-codes GMT as the time zone.
-// You should use this function for the "Date" and "Last-Modified"
-// headers.
-func FormatTime(t time.Time) string {
-	s := t.UTC().Format(time.RFC1123)
-	s = s[:len(s)-3] + "GMT"
-	return s
-}
+// // FormatTime formats time according to the HTTP spec.
+// // It is like time.RFC1123 but hard-codes GMT as the time zone.
+// // You should use this function for the "Date" and "Last-Modified"
+// // headers.
+// func FormatTime(t time.Time) string {
+// 	s := t.UTC().Format(time.RFC1123)
+// 	s = s[:len(s)-3] + "GMT"
+// 	return s
+// }
 
-// MIMETypeByExtension returns the MIME type associated with the
-// file extension ext. The extension ext should begin with a
-// leading dot, as in ".html". When ext has no associated type,
-// MIMETypeByExtension returns "".
-// You should use this function for the "Content-Type" header.
-func MIMETypeByExtension(ext string) string {
-	return mime.TypeByExtension(ext)
-}
+// // MIMETypeByExtension returns the MIME type associated with the
+// // file extension ext. The extension ext should begin with a
+// // leading dot, as in ".html". When ext has no associated type,
+// // MIMETypeByExtension returns "".
+// // You should use this function for the "Content-Type" header.
+// func MIMETypeByExtension(ext string) string {
+// 	return mime.TypeByExtension(ext)
+// }
 
 // ReadLine reads a single line ending with "\r\n" from br,
 // striping the "\r\n" line end from the returned string.
